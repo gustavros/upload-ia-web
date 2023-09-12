9f6dec61044505d2a714cf10e406aea9e26ef682
Gustavo Santana <gustavossw@hotmail.com>
2023-09-12 10:20:46 -0300

    feat: ui application created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,160 @@
-import React from "react";
+import { Github, FileVideo, Upload, Wand2 } from "lucide-react";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import { Button } from "./components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
 
-const App = () => {
-  return <h1>Hello World</h1>;
-};
+export function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <header className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
 
-export default App;
+        <div className="flex items-center gap-3">
+          <span className="text-muted-foreground">
+            Desenvolvido com 💛 por{" "}
+            <a href="https://gusta.live" className="text-primary">
+              Gustavo Santana
+            </a>
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <a
+            className="flex items-center rounded-lg h-9 px-4 py-2 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground"
+            href="https://github.com/gustavros"
+          >
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </a>
+        </div>
+      </header>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-5"
+              placeholder="Resultado gerado pela IA"
+              readOnly
+            />
+          </div>
+
+          <p className="text-muted-foreground text-sm">
+            Lembre-se: você pode utilizar a viarável{" "}
+            <code className="text-amber-500">{"{transcription}"} </code>
+            no seu prompt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
+          </p>
+        </div>
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              className="border flex flex-col gap-2 items-center justify-center rounded-md aspect-video cursor-pointer border-dashed text-sm text-muted-foreground hover:bg-primary/5"
+              htmlFor="video"
+            >
+              <FileVideo />
+              Selecione um vídeo
+            </label>
+
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                className="h-20 leading-relaxed resize-none"
+                id="transcription_prompt"
+                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+
+          <Separator />
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt..." />
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectItem value="title">Título do Youtube</SelectItem>
+                  <SelectItem value="description">
+                    Descrição do Youtube
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+
+              <Select disabled defaultValue="gpt3.5">
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+
+              <p className="text-xs text-muted-foreground italic">
+                Você poderá costumizar essa opção em breve
+              </p>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label>Temperatura</Label>
+
+              <Slider min={0} max={1} step={0.1} />
+
+              <p className="text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros.
+              </p>
+            </div>
+
+            <Separator />
+
+            <Button className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
+  );
+}
